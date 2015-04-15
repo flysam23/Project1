1035c12784916feb22575c19a1df7fcfae3e10db
flysam23 <d_shantanu2003@yahoo.com>
2015-04-15 14:51:07 -0400

    addEmployee

---
+++ Employee.R
@@ -0,0 +1,8 @@
+Name <- (c("Becky","Betsy","Bill","Bob"))
+SalaryFY13 <- (c(120000, 55000, 58000, 110000))
+BonusFY13 <- (c(10000, 0, 5000, 10000))
+SalaryFY14 <- (c(125000, 65000, 60000, 125000))
+BonusFY14 <- (c(20000, 3000, 3000, 15000))
+payrollData <- data.frame(Name, SalaryFY13, BonusFY13, SalaryFY14
+                          , BonusFY14)
+payrollData
